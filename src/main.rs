f20a84ba173b74b8422287736ada99c14109e653
Zeadar <siiigsonik@gmail.com>
2024-11-21 14:56:06 +0100

    bump

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,13 @@ use std::{
     net::{TcpListener, TcpStream},
 };
 
+const ADDR: &str = "127.0.0.1";
+const PORT: &str = "7878";
+
 #[derive(Deserialize, Serialize, Debug)]
 struct Settings {
     root_path: String,
 }
-
-const ADDR: &str = "127.0.0.1";
-const PORT: &str = "7878";
 
 fn main() {
     let settings = fs::read_to_string("settings.json").unwrap();
@@ -39,8 +39,25 @@ fn handle_connection(mut stream: TcpStream, settings: &Settings) {
 
     println!("Request: {http_request:#?}");
 
+    let request_line: Vec<&str> = http_request[0].split(" ").collect();
+
+    let request_type = request_line[0];
+    let request_path = request_line[1];
+    let request_version = request_line[2];
+
+    // let accept_line = http_request
+
+    println!(
+        "type {}, path {}, version {}",
+        request_type, request_path, request_version
+    );
+
     let status_line = "HTTP/1.1 200 OK";
-    let content = fs::read_to_string(format!("{}/index.html", settings.root_path)).unwrap();
+    let content = match request_path {
+        "/" => fs::read_to_string(format!("{}/index.html", settings.root_path)).unwrap(),
+        _ => fs::read_to_string(format!("{}{}", settings.root_path, request_path)).unwrap(),
+    };
+
     let content_len = content.len();
 
     let response = format!("{status_line}\r\nContent-length: {content_len}\r\n\r\n{content}");
