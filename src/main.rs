dccf1d00e03c67c1b30b84e4233f55e9eb264907
Zeadar <siiigsonik@gmail.com>
2024-11-29 11:54:57 +0100

    404

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,6 @@ struct Settings {
     bind_port: String,
 }
 
-struct Response {
-    type_text: String,
-    content: String,
-}
-
 const SETTINGS_PATH: &str = "settings.json";
 
 fn main() {
@@ -70,18 +65,6 @@ fn handle_connection(mut stream: TcpStream, settings: &Settings) {
     }
 
     let request_line: Vec<&str> = http_request[0].split(" ").collect();
-    let accept: Vec<&String> = http_request
-        .iter()
-        .filter(|s| s.contains("Accept:"))
-        .collect();
-
-    let accept_text = match accept.first() {
-        Some(s) => s.contains("text"),
-        _ => {
-            println!("No Accept line in Request");
-            return;
-        }
-    };
 
     let request_type = request_line[0];
     let request_path = match request_line[1] {
@@ -100,53 +83,53 @@ fn handle_connection(mut stream: TcpStream, settings: &Settings) {
         return;
     }
 
-    if accept_text {
-        let response: Response =
-            match fs::read_to_string(format!("{}/{request_path}", settings.root_path)) {
-                Ok(content) => Response {
-                    type_text: String::from("200 OK"),
-                    content,
-                },
-                Err(err) => {
-                    println!("{err}");
-                    Response {
-                        type_text: String::from("404 NOT FOUND"),
-                        content: fs::read_to_string("404.html").unwrap(),
-                    }
-                }
-            };
+    let path = format!("{}{request_path}", settings.root_path);
+    let file_data = match fs::read(&path) {
+        Ok(data) => data,
+        Err(err) => {
+            println!("{err}");
+            let content404 = content_404(err.to_string());
+            let content404_len = content404.len();
+            let response = format!(
+                "HTTP/1.1 404 NOT FOUND\r\ncontent-length: {content404_len}\r\n\r\n{content404}"
+            );
+            stream.write_all(response.as_bytes()).unwrap();
+            return;
+        }
+    };
 
-        let status_line = format!("HTTP/1.1 {}", response.type_text);
-        let content_len = response.content.len();
-        let response = format!(
-            "{status_line}\r\nContent-length: {content_len}\r\n\r\n{}",
-            response.content
-        );
-        stream.write_all(response.as_bytes()).unwrap();
-    } else {
-        let path = format!("{}{request_path}", settings.root_path);
-        let file_data = match fs::read(&path) {
-            Ok(data) => data,
-            Err(err) => {
-                println!("{err}");
-                let content404 = fs::read_to_string("404.html").unwrap();
-                let content404_len = content404.len();
-                let response = format!("HTTP/1.1 404 NOT FOUND\r\ncontent-length: {content404_len}\r\n\r\n{content404}");
-                stream.write_all(response.as_bytes()).unwrap();
-                return;
-            }
-        };
+    let mime = mime_guess::from_path(&path)
+        .first_or_octet_stream()
+        .to_string();
+    println!("Guessed {mime} from {path}");
+    let header = format!(
+        "HTTP/1.1 200 OK\r\nContent-Type: {}\r\nContent-Length: {}\r\n\r\n",
+        mime,
+        file_data.len()
+    );
+    stream.write_all(header.as_bytes()).unwrap();
+    stream.write_all(file_data.as_slice()).unwrap();
+}
 
-        let mime = mime_guess::from_path(&path)
-            .first_or_octet_stream()
-            .to_string();
-        println!("Guessed {mime} from {path}");
-        let header = format!(
-            "HTTP/1.1 200 OK\r\nContent-Type: {}\r\nContent-Length: {}\r\n\r\n",
-            mime,
-            file_data.len()
-        );
-        stream.write_all(header.as_bytes()).unwrap();
-        stream.write_all(file_data.as_slice()).unwrap();
-    }
+fn content_404(message: String) -> String {
+    let first = r#"
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Hello!</title>
+  </head>
+  <body>
+    <h1>Oops!</h1>
+      <p>Sorry, I don't know what you're asking for.</p>
+    <div>
+    <p style="background-color: beige; padding: 4px;">
+    "#;
+    let second = r#"
+    </p>
+    </div>
+  </body>
+</html>
+    "#;
+    return format!("{first}{message}{second}");
 }
